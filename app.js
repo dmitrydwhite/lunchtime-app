d9ce0dc738c4c8c3cc95abe92bf20ed05159f23a
Carlo De Gregorio <cdegregorio@gmail.com>
2014-06-12 11:03:00 -0700

    added request url fxn

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
 $(document).ready( function () {
   'use strict';
+  
+   var requestFromFourSquare = function() {
+    var data = {
+      client_id: 'BBXQZQD1XYLBS1MI3UNLIQMKFMEGYSBTYAZVYEMPRKPAPCNO',
+      client_secret: 'SCRYCL2WJIORP054NIIFFPQ0KTL4OMMHNN3TMRS32FDSX15P',
+      ll: getLocation(),
+      section: 'food',
+      limit: 1,
+      venuePhoto: 1,
+      format: 'json'
+    };
+
+   	var url = 'https://api.foursquare.com/v2/venues/search';
+    $.ajax(url, { data: data, dataType: 'jsonp', jsonp: 'jsoncallback' })
+      .then(function(data, status, xhr) {
+        counter += 1;
+        maxPages = data.photos.pages;
+
+        displayPhotos(data, status, xhr);
+      }, function(xhr, status, error){
+
+      });
+  };
+
+
+
+
+
   findLunch();
 });
