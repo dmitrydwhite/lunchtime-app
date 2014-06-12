7b34525e4d67053cdd2b1d338bfc51211929137c
Dmitry White <white.dmitry@gmail.com>
2014-06-12 12:46:36 -0700

    Displays the name of a nearby food establishment in plain text.

--- app.js
+++ app.js
@@ -1,47 +1,58 @@
 $(document).ready( function () {
   'use strict';
 
-   var requestFromFourSquare = function() {
+  var displayVenues = function (data, status, xhr) {
+    console.log('getting to display venues');
+    $('<p>', {
+      text: data.response.groups[0].items[0].venue.name
+    }).prependTo('body');
+  };
+
+   var requestFromFourSquare = function(loc) {
+    console.log('starting foursquare request');
     var data = {
       client_id: 'BBXQZQD1XYLBS1MI3UNLIQMKFMEGYSBTYAZVYEMPRKPAPCNO',
       client_secret: 'SCRYCL2WJIORP054NIIFFPQ0KTL4OMMHNN3TMRS32FDSX15P',
-      ll: getLocation(),
+      ll: loc,
       section: 'food',
       limit: 1,
       venuePhoto: 1,
+      v: '20140612',
       format: 'json'
     };
 
-   	var url = 'https://api.foursquare.com/v2/venues/search';
-    $.ajax(url, { data: data, dataType: 'jsonp', jsonp: 'jsoncallback' })
-      .then(function(data, status, xhr) {
-        counter += 1;
-        maxPages = data.photos.pages;
 
-        displayPhotos(data, status, xhr);
-      }, function(xhr, status, error){
+   	var url = 'https://api.foursquare.com/v2/venues/explore';
+
+    $.ajax(url, { data: data, dataType: 'jsonp' })
+      .then(function(data, status, xhr) {
+        displayVenues(data, status, xhr);
+      }, function(xhr, status, error) {
 
       });
-  };
-
+    };
   var getLocation = function () {
-    function requestLocation() {
+    console.log('getting location');
+    var requestLocation = function() {
+      console.log('requesting location');
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
-          return revealPosition(position);
+          revealPosition(position);
         });
-      } else {
+        }
+       else {
         $('<p>', {text: 'Geo-Location does not appear ' +
           'to be supported at this time'}).prependTo('body');
       }
-    }
+    };
+
 
     function revealPosition(position) {
       var positionString = '';
       var latString = position.coords.latitude.toString().slice(0,6);
       var longString = position.coords.longitude.toString().slice(0,7);
       positionString = latString + ',' + longString;
-      return positionString;
+      requestFromFourSquare(positionString);
     }
 
     requestLocation();
@@ -49,7 +60,6 @@ $(document).ready( function () {
 
   var findLunch = function () {
     getLocation();
-    requestFromFourSquare();
   };
 
 
